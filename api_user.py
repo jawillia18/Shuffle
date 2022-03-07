86166aa80a3cd357fd0514bd892c75f18c47c35d
Jayden Williams <jawillia@student.wethinkcode.co.za>
2022-03-07 11:58:46 +0200

    -

--- api_user.py
+++ api_user.py
@@ -9,53 +9,53 @@ import datetime
 BASE = "http://127.0.0.1:5000"
 
 
-def get_current_day():
-    today = datetime.date.today()
-    return today.strftime("%Y-%m-%d")
+# def get_current_day():
+#     today = datetime.date.today()
+#     return today.strftime("%Y-%m-%d")
 
-def get_current_id():
-    
+# def get_current_id():
+#     return null
 
 
-@app.route("/view", methods=["GET"])
-def on_view():
-    try:
-        day_id = get_current_day() # get our day_id, which is the date string in the format "yyyy-mm-dd"
-        client_ip = request.remote_addr # get the ip address of where the client request came from
+# @app.route("/view", methods=["GET"])
+# def on_view():
+#     try:
+#         day_id = get_current_day() # get our day_id, which is the date string in the format "yyyy-mm-dd"
+#         client_ip = request.remote_addr # get the ip address of where the client request came from
 
-        query = Day.query.filter_by(id=day_id) # try to get the row associated to the current day
-        if query.count() > 0: 
-            # the current day is already in table, simply increment its views
-            current_day = query.first()
-            current_day.views += 1
-        else:
-            # the current day does not exist, its the first view for the day.
-            current_day = Day(day_id, 1, 0)
-            db.session.add(current_day) # insert a new day into the day table
+#         query = Day.query.filter_by(id=day_id) # try to get the row associated to the current day
+#         if query.count() > 0: 
+#             # the current day is already in table, simply increment its views
+#             current_day = query.first()
+#             current_day.views += 1
+#         else:
+#             # the current day does not exist, its the first view for the day.
+#             current_day = Day(day_id, 1, 0)
+#             db.session.add(current_day) # insert a new day into the day table
         
-        query = IpView.query.filter_by(ip=client_ip,date_id=day_id)
-        if query.count() == 0: # check if its the first time a viewer from this ip address is viewing the website
-            ip_view = IpView(client_ip, day_id)
-            db.session.add(ip_view) # insert into the ip_view table
+#         query = IpView.query.filter_by(ip=client_ip,date_id=day_id)
+#         if query.count() == 0: # check if its the first time a viewer from this ip address is viewing the website
+#             ip_view = IpView(client_ip, day_id)
+#             db.session.add(ip_view) # insert into the ip_view table
         
-        db.session.commit() # commit all the changes to the database
-        return {}, 200
-    except Exception as e:
-        return {"error":True, "message": str(e)}, 500
+#         db.session.commit() # commit all the changes to the database
+#         return {}, 200
+#     except Exception as e:
+#         return {"error":True, "message": str(e)}, 500
 
-@app.route("/read", methods=["GET"])
-def on_read():
-    try:
-        day_id = get_current_id()
-        query = Day.query.filter_by(id=day_id)
-        if query.count() == 0:
-            return {"error": True, "message": f"Day {day_id} does not exist"}, 400
-        day = query.first()
-        day.reads += 1
-        db.session.commit()
-        return {}, 200
-    except Exception as e:
-        return {"error": True, "message":str(e)}, 500
+# @app.route("/read", methods=["GET"])
+# def on_read():
+#     try:
+#         day_id = get_current_id()
+#         query = Day.query.filter_by(id=day_id)
+#         if query.count() == 0:
+#             return {"error": True, "message": f"Day {day_id} does not exist"}, 400
+#         day = query.first()
+#         day.reads += 1
+#         db.session.commit()
+#         return {}, 200
+#     except Exception as e:
+#         return {"error": True, "message":str(e)}, 500
 
 
 response = requests.get(BASE + "/episode/<int:id>")
--- app.py
+++ app.py
@@ -1,23 +1,27 @@
+from pickle import FALSE
+from django import db
 from flask import Flask, render_template, g
 from flask_restful import Api, Resource, reqparse
-import mysql.connector
-from flask_sqlalchemy import SQLAlchemy
-from config import DATABASE_URI
+from flask_track_usage.storage.sql import SQLStorage
+from flask_sqlalchemy import  SQLAlchemy
+
+
 
 app = Flask(__name__) #Create Flask object app
 
-app.config['TRACK_USAGE_USE_FREEGEOIP'] = False
-app.config['TRACK_USAGE_INCLUDE_OR_EXCLUDE_VIEWS'] = 'include'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://postgres:password12@localhost:5000/analytics"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+sql_db = SQLAlchemy(app)
 from flask_track_usage import TrackUsage
 
 from flask_track_usage.storage.printer import PrintWriter
 from flask_track_usage.storage.output import OutputWriter
 api = Api(app) #creating api object wrapping app as API
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
-db = SQLAlchemy(app)
-t = TrackUsage(app, [
-    PrintWriter(),
-    OutputWriter(transform=lambda s: "OUTPUT: " + str(s))
+
+pstore= SQLStorage(db= sql_db)
+t = TrackUsage(app, [pstore
+    # PrintWriter(),
+    # OutputWriter(transform=lambda s: "OUTPUT: " + str(s))
 ])
 
 episode_args = reqparse.RequestParser() #creating arguments for the episode 
@@ -27,10 +31,10 @@ episode_args.add_argument(name="listens", type=int, help="Number of listens")
 episode_args.add_argument(name="likes", type=int, help="Number of likes")
 
 
-episodes = {1:{"episode_name":"How to build a car","discription":"This is an episode on how to build your own car using boxes and glue", "listens":15, "likes":7},
-            2:{"episode_name":"How to build a car","discription":"This is an episode on how to build your own car using boxes and glue", "listens":145, "likes":7},
-            3:{"episode_name":"How to build a car","discription":"This is an episode on how to build your own car using boxes and glue", "listens":59, "likes":7},
-            4:{"episode_name":"How to build a car","discription":"This is an episode on how to build your own car using boxes and glue", "listens":1500, "likes":7}}
+episodes = {1:{"episode_name":"Playlist_1","discription":"...", "listens":15, "likes":7},
+            2:{"episode_name":"Playlist_2","discription":"...", "listens":145, "likes":7},
+            3:{"episode_name":"Playlist_3","discription":"...", "listens":59, "likes":7},
+            4:{"episode_name":"Playlist_4","discription":"...", "listens":1500, "likes":7}}
             # 5:{"episode_name":"How to build a car","discription":"This is an episode on how to build your own car using boxes and glue", "listens":515, "likes":7},
             # 6:{"episode_name":"How to build a car","discription":"This is an episode on how to build your own car using boxes and glue", "listens":786, "likes":7},
             # 7:{"episode_name":"How to build a car","discription":"This is an episode on how to build your own car using boxes and glue", "listens":2021, "likes":7},
